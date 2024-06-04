48cca6a823ceac0525893c198400d758da5f195e
seamusic <seamusic43@gmail.com>
2024-06-05 08:19:50 +0900

    font style change

--- src/components/ui/PageTitle.jsx
+++ src/components/ui/PageTitle.jsx
@@ -1,7 +1,7 @@
 
 export default function PageTitle({ title }) {
     return (
-        <div className="font-bold font-xl p-4">
+        <div className="font-bold font-xl p-4 nanum-brush-script-regular">
             <h1>{title}</h1>
         </div>
     )

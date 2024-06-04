6efa05ece196cffaeffede7e820b5f4c00174d88
seamusic <seamusic43@gmail.com>
2024-06-04 13:21:28 +0900

    business validate check function add

--- src/pages/VendorJoin.jsx
+++ src/pages/VendorJoin.jsx
@@ -4,6 +4,52 @@ import PageTitle from "@/components/ui/PageTitle"
 export default function VendorJoin() {
 
 
+    /*
+    const useReducer = (reducer, initialState) => {
+    }
+
+    */
+    const TryJoin = (e) => {
+        e.preventDefault();
+        console.log('TryJoin');
+        const form = document.forms.vendor_join_form;
+        const formData = new FormData(form);
+        for (let key of formData.keys()) {
+            if (key === 'business_number') {
+                if (validate_business_number(formData.get(key))) {
+                    console.log('business_number is valid');
+                } else {
+                    console.log('business_number is invalid');
+                    //setCheckError({ [key]: 'wrong businees number' });
+                    return false;
+                }
+            }
+            console.log(key, formData.get(key));
+        }
+    }
+
+    const validate_business_number = (business_number) => {
+        // Business number validation
+        const regex = new RegExp('^[0-9]{3}-[0-9]{2}-[0-9]{5}$');
+        if (regex.test(business_number)) {
+            const valueMap = business_number.replace(/-/gi, '').split('').map(function (item) {
+                return parseInt(item, 10);
+            })
+            if (valueMap.length == 10) {
+                const multipliers = [1, 3, 7, 1, 3, 7, 1, 3, 5];
+                let checkSum = 0;
+                for (let i = 0; i < 9; i++) {
+                    checkSum += valueMap[i] * multipliers[i];
+                }
+                checkSum += Math.floor(valueMap[8] * 5 / 10);
+                return Math.floor(10 - (checkSum % 10)) === valueMap[9];
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
 
     return (
         <div className="flex justify-center">
@@ -11,7 +57,7 @@ export default function VendorJoin() {
                 <PageTitle title="Vendor Join" />
                 <form name="vendor_join_form">
                     <CommonJoinForm />
-                    <button className="btn btn-primary" type="submit">Join</button>
+                    <button onClick={TryJoin} className="btn btn-primary" type="submit">Join</button>
                 </form></div>
         </div>
     )
--- src/pages/common/JoinProperty.jsx
+++ src/pages/common/JoinProperty.jsx
@@ -5,7 +5,7 @@ export const JoinProperty = [
     { 'name': 'email', 'type': 'email', 'placeholder': 'enter the email', required: true, pattern: '^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$' }, //'^([\\.w-]+(?:\\.[\\.w-]+)*)@((?:[\\.w-]+\\.)*\\.w[\\.w-]{0,66})\\.([a-z]{2,6}(?:\\.[a-z]{2})?)$' },
     { 'name': 'phone', 'type': 'tel', 'placeholder': 'enter the phone number', required: true, pattern: '^01(?:0|1|[6-9])(?:\\.{3}|\\.{4})\\.{4}$', },
     { 'name': 'company_name', 'type': 'text', 'placeholder': 'enter the company name', required: true, minlength: 2, maxlength: 20 },
-    { 'name': 'business_number', 'type': 'text', 'placeholder': 'enter the business number', required: true, pattern: '/^([0-9]{3})-([0-9]{2})-([0-9]{5})$/i', minlength: 12, maxlength: 12 },
+    { 'name': 'business_number', 'type': 'text', 'placeholder': 'enter the business number', required: true, pattern: '^([0-9]{3})-([0-9]{2})-([0-9]{5})$', minlength: 12, maxlength: 12 },
     { 'name': 'all_agree', 'type': 'checkbox', 'label': 'All agree' },
     { 'name': 'service_agree', 'type': 'checkbox', 'label': 'Service agree', required: true },
     { 'name': 'privacy_agree', 'type': 'checkbox', 'label': 'Privacy agree', required: true },

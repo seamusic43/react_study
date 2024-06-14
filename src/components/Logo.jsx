48ba7990087d03f7c9dedf2395a2f9ddab84431d
seamusic <seamusic43@gmail.com>
2024-06-14 16:56:05 +0900

    add Logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Logo() {
+    return (
+        <div className="flex">
+            <img src="./public/coggiri_logo2_100.png" />
+            <img src="./public/logo_text.png" />
+        </div>
+    );
+}
